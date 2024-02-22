9fdb6eb48a624ed8a6f72480a815278291eaadd0
Anatol Kaczmarek <anatol.kaczmarek@gmail.com>
2024-02-22 22:11:03 +0100

    admin site added

--- network/models.py
+++ network/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts')
